f1bd4745e81cc30268c4d0112dd72349db3a934e
fritziyeke12 <fritziyeke@gmail.com>
2024-11-12 22:37:17 +0000

    Added new page with routing

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Page(){
+    return (
+        <>
+            <h1><b>This is the dashboard page!!!!!!!!!!!!!</b></h1>
+            <Link href="/">Go to home</Link>
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function Page(){
-  return <h1>This is the first page</h1>
+  return (
+    <>
+      <h1>This is the first page</h1>
+      <Link href="/dashboard">Go to the second page</Link>
+    </>
+  )
 }
